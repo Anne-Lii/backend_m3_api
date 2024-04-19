ab6896f5da355e52329b85ca1ffc780e3c9269fc
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-04-19 08:23:22 +0200

    fått till koppling till databasen Mongo DB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //include packages
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 //init express
 const app = express();
@@ -10,6 +11,13 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+//connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/cv").then(()=> {
+    console.log("connected to MongoDB");
+}).catch((err) => {
+    console.log("Error while connecting to database: " + err);
+});
 
 //Routes
 app.get("/api", async (req, res) => {

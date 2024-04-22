f4d5825ab5157687aeece191cb5b3d525ad256ab
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-04-22 08:11:40 +0200

    fått upp dataservern i molntjänsten Atlas

--- server.js
+++ server.js
@@ -4,23 +4,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config({ path: './.env' });
 
 //init express
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const localUrl = "mongodb://127.0.0.1:27017/jobs";
-const externalUrl = "mongodb://51.140.210.101:27017/jobs"; // Använd din virtuella IP-adress här
-
 //connect to MongoDB
-mongoose.connect(externalUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGO_URI, {
+
 }).then(() => {
-    console.log("connected to MongoDB");
+    console.log("connected to MongoDB", mongoose.connection.db.databaseName);
 }).catch((err) => {
     console.log("Error while connecting to database: " + err);
 });
@@ -29,41 +26,41 @@ mongoose.connect(externalUrl, {
 //schedule for jobs
 const jobSchedule = new mongoose.Schema({
     companyname: {
-        type: String, 
+        type: String,
         required: [true, "Fyll i företagets namn"]
     },
     location: {
-        type: String, 
+        type: String,
         required: [true, "Fyll i stad"]
     },
     jobtitle: {
-        type: String, 
+        type: String,
         required: [true, "Fyll i jobbtitel"]
     },
     describtion: {
-        type: String, 
+        type: String,
         required: false
     },
     stardate: {
-        type: Date, 
+        type: Date,
         required: false
     },
     enddate: {
-        type: Date, 
+        type: Date,
         required: false
     }
 });
 
 //include schedule
-const Job = mongoose.model("Job", jobSchedule );
+const Job = mongoose.model("Job", jobSchedule);
 
 //Routes
 app.get("/", async (req, res) => {
-res.json({message: " Välkommen"});
+    res.json({ message: " Välkommen till mitt API" });
 });
 
 //GET
-app.get ("/jobs", async (req, res) => {
+app.get("/jobs", async (req, res) => {
     try {
         let result = await Job.find({});
         return res.json(result);
@@ -73,36 +70,36 @@ app.get ("/jobs", async (req, res) => {
 });
 
 //POST
-app.post ("/jobs", async (req, res) => {
-   try {
+app.post("/jobs", async (req, res) => {
+    try {
         let result = await Job.create(req.body);
         return res.json(result);
-   } catch (err) {
+    } catch (err) {
         return res.status(400).json(err);
-}
+    }
 });
 
 //PUT
-app.put ("/jobs/:id", async (req, res) => {
+app.put("/jobs/:id", async (req, res) => {
     try {
-         let job = await Job.findByIdAndUpdate(req.params.id, req.body, { new:true});
-         return res.json(job);
+        let job = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.json(job);
     } catch (err) {
-         return res.status(400).json(err);
-}
+        return res.status(400).json(err);
+    }
 });
 
 //DELETE
 app.delete("/jobs/:id", async (req, res) => {
     try {
-         await Job.findByIdAndDelete(req.params.id);
-         return res.json({message: "Jobb raderat"});
+        await Job.findByIdAndDelete(req.params.id);
+        return res.json({ message: "Jobb raderat" });
     } catch (err) {
-         return res.status(500).json(err);
-}
+        return res.status(500).json(err);
+    }
 });
 
 //run app
 app.listen(port, () => {
-console.log("Server is running on port: " + port);
+    console.log("Server is running on port: " + port);
 });
